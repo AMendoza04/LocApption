d62a7ed447532a56bb718f35b737cb1eaf8e0506
AMendoza04 <daniel.mendo04@gmail.com>
2020-03-18 21:07:41 -0500

    Permissions

--- app/src/main/java/com/example/locapption/MainActivity.java
+++ app/src/main/java/com/example/locapption/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.locapption;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton ibtn_cam;
+    ImageButton ibtn_contacts;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ibtn_cam = findViewById( R.id.ibtn_cam );
+        ibtn_contacts = findViewById( R.id.ibtn_contacts );
+
+        ibtn_contacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ibtn_cam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
